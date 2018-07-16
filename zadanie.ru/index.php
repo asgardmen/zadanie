f409098ed622fd6e382532f479b357a270b1db98
asgardmen <asgardmen@yandex.ru>
2018-07-16 19:14:45 +0400

    Update index.php

--- zadanie.ru/index.php
+++ zadanie.ru/index.php
@@ -28,7 +28,7 @@
 			</div>
 			<div class="row">
 				<div class="col-lg-2"></div>
-				<div class="col-lg-7 jim_button"><a class="btn zakaz_button" href="#">ЗАКАЗАТЬ ЗВОНОК</a></div>
+				<div class="col-lg-7 jim_button"><a class="btn zakaz_button" href="#form_inner">ЗАКАЗАТЬ ЗВОНОК</a></div>
 				<div class="col-lg-2"></div>
 			</div>
 
